d6ce738be02d64d87206af26e4d1a7e8a165c117
zak-info <z.benslite@esi-sba.dz>
2025-10-19 11:09:04 +0100

    feat: add changes

--- src/components/Documentation/Introduction.tsx
+++ src/components/Documentation/Introduction.tsx
@@ -1,11 +1,5 @@
 "use client"
 import Image from "next/image"
-import nextImg from "/public/images/documentation/Categories=Nextjs.svg"
-import reactImg from "/public/images/documentation/Categories=React.svg"
-import tailwindImg from "/public/images/documentation/Categories=Tailwind.svg"
-import nextauthImg from "/public/images/documentation/nextauth.png"
-import typescriptImg from "/public/images/documentation/Categories=Typescript.svg"
-import axiosImg from "/public/images/documentation/axios.svg"
 import { Icon } from "@iconify/react/dist/iconify.js"
 import { useState } from "react"
 import { DocNavigation } from "./DocNavigation"
@@ -66,7 +60,7 @@ export const Introduction = () => {
         </div>
         <div className="mt-5">
           <p className="text-base font-medium text-midnight_text dark:text-white dark:text-opacity-50">El Nour School Management System is built with Tailwindcss and Nextjs.</p>
-          <p className="text-base font-medium text-midnight_text dark:text-white dark:text-opacity-50">This system is ready to use and you can totally customize as per your school's requirements.</p>
+          <p className="text-base font-medium text-midnight_text dark:text-white dark:text-opacity-50">This system is ready to use and you can totally customize as per your school&apos;s requirements.</p>
           <p className="text-base font-medium text-midnight_text dark:text-white dark:text-opacity-50">For customization, you should have knowledge of NextJs, ReactJs, Tailwind and JSX to be able to modify this system.</p>
         </div>
 
